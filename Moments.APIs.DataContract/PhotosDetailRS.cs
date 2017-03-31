fab14f98b946b530b34bebc0cc9a5e89fc233d01
Akhilesh Singh Chauhan <achauhan@internal.tavisca.com>
2017-03-31 20:30:35 +0530

    Added photo details model.

--- Moments.APIs.DataContract/PhotosDetailRS.cs
+++ Moments.APIs.DataContract/PhotosDetailRS.cs
@@ -1,7 +1,13 @@
-﻿namespace Moments.APIs.DataContract
+﻿using System.Collections.Generic;
+
+namespace Moments.APIs.DataContract
 {
     public class PhotosDetailRS
     {
-
-    }
+        public int TotalImages { get; set; }
+        public int PageSize { get; set; } //default will be 10
+        public string ImageUrl { get; set; }
+        public List<string> FriendsList { get; set; }
+        public string CollectionId { get; set; }
+      }
 }
