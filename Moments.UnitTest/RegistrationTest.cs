50c225423dc24d8495c9f01630a379eeabda8a86
Akhilesh Singh Chauhan <achauhan@internal.tavisca.com>
2017-04-01 16:41:15 +0530

    Added test case for registration api on core level.

---
+++ Moments.UnitTest/RegistrationTest.cs
@@ -0,0 +1,45 @@
+﻿using Moments.APIs.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Moments.APIs.DataContract;
+
+namespace Moments.UnitTest
+{
+    public class RegistrationTest
+    {
+        [Fact]
+        public async void TestCoreRegistrationAPI()
+        {
+            var registration = new Registration();
+            var response = await registration.SaveUserData(GetUserRequest());
+        }
+
+        private UserRegistrationRQ GetUserRequest()
+        {
+            return new UserRegistrationRQ()
+            {
+                User = new User()
+                {
+                    Name = new UserName()
+                    {
+                        LastName = "Pawar",
+                        FirstName = "Rahul",
+                        MiddleName = "M"
+                    },
+                    Email = "meetrahul8@gmail.com",
+                    Gender = Gender.Male,
+                    FriendsList = new List<string>()
+                    {
+                        "10001",
+                        "10002"
+                    },
+                    UserId = "100"
+                },
+            };
+        }
+    }
+}
